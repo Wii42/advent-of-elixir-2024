d1c6a02c26316a2097840144b10e478de8fd6e90
Wi42 <101128608+Wii42@users.noreply.github.com>
2024-12-01 12:19:13 +0100

    day 1

---
+++ lib/day_1/1_1.ex
@@ -0,0 +1,43 @@
+defmodule AdventOfCode2024.Day1_1 do
+  @moduledoc """
+  Documentation for `AdventOfCode2024`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode2024.hello()
+      :world
+
+  """
+  def test() do
+    {:ok, input} = File.read("1_test_input.txt")
+    solution(input) |> IO.inspect()
+  end
+
+  def prod() do
+    {:ok, input} = File.read("1_input.txt")
+    solution(input) |> IO.inspect()
+  end
+
+  def solution(input) do
+    lines = String.split(input, "\r\n")
+    first_part = for line <- lines, do: get_nth_elem(line, 0)
+    second_part = for line <- lines, do: get_nth_elem(line, 1)
+    first_part = Enum.sort(first_part)
+    second_part = Enum.sort(second_part)
+    IO.inspect(first_part)
+    IO.inspect(second_part)
+    IO.inspect(Enum.zip(first_part, second_part))
+    Enum.reduce(Enum.zip(first_part, second_part), 0, fn {a, b}, acc -> abs(a - b) + acc end)
+  end
+
+  defp get_nth_elem(line, index) do
+    parts = String.split(line, " ", trim: true)
+    Enum.at(parts, index) |> String.to_integer()
+  end
+end
+
+AdventOfCode2024.Day1_1.prod()
